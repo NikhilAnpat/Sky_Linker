970bcbdbfd64c8be9bc3f1192eb97cfbc49db7d6
NikhilAnpat <111105422+NikhilAnpat@users.noreply.github.com>
2023-08-31 07:52:53 +0530

    Delete src directory

--- src/main/java/com/app/config/WebSecurityCfgCustom.java
+++
@@ -1,7 +0,0 @@
-//package com.app.config;
-//
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//public class WebSecurityCfgCustom extends WebSecurityConfigurerAdapter{
-//	
-//}
--- src/main/java/com/app/pojos/Role.java
+++
@@ -1,6 +0,0 @@
-package com.app.pojos;
-
-public enum Role {
-	ADMIN, MANAGER, CUSTOMER
-	,ROLE_CUSTOMER,ROLE_MANAGER,ROLE_ADMIN
-}
